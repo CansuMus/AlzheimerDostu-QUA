7e5ce499e80c8490699a848573e94c8821421de8
Abdoul <abdoulrachidsdg@gmail.com>
2023-06-15 07:28:56 +0300

    Splash, login and register page v1

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:ff_55/routes/app_routes.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp.router(
+      routerConfig: AppRoutes.router,
+      // title: 'Flutter Demo',
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple,
+      //  ),
+      //   useMaterial3: true,
+      // ),
+      
+    );
+  }
+}
---
+++ lib/styles/colors/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Utils{
+  static const Color mainThemeColor = Color(0xff263f9b);
+}
